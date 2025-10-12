6375ad66a3aa97b0b5ea41634a91813831accc72
2224475LN <222447583@edu.vut.ac.za>
2025-10-12 20:26:38 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Column names
+FEATURE_COLUMNS = ["schizophrenia", "anxiety", "bipolar", "eating"]
+LABEL_COLUMN = "high_depression"
+
+# File paths
+DATA_FILE = "1- mental-illnesses-prevalence.csv"
+HISTORY_FILE = "History.csv"
+NEW_PATIENTS_PATH = "NewPatients.csv"
