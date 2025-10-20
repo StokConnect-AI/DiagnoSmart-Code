ecfb9f8d61392d75482b1797079af4161d82faca
Moo-re26 <126968502+Moo-re26@users.noreply.github.com>
2025-10-20 20:58:43 +0200

    Update model_randomforest.py
    comment updated

--- model_randomforest.py
+++ model_randomforest.py
@@ -1,11 +1,11 @@
-'''
-This Python script defines a function that trains a Random Forest classifier using 200 decision trees on the provided training data. 
-The model is initialized with a fixed random seed for reproducibility, trained on the input features and labels, and then returned for future predictions or evaluation.
-'''
+#This Python script defines a function that trains a Random Forest classifier using 200 decision trees on the provided training data. 
+#The model is initialized with a fixed random seed for reproducibility, trained on the input features and labels, and then returned for future predictions or evaluation.
+
 from sklearn.ensemble import RandomForestClassifier
 
 def train_random_forest(X_train, y_train):
     rf = RandomForestClassifier(n_estimators=200, random_state=42)
     rf.fit(X_train, y_train)
     return rf
+
 
